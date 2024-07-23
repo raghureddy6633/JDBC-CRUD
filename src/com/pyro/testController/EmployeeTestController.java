01564b58876fd6f777ee4502f4843d2a8a9ee970
Raghureddy6633 <gondhiraghava305@gmail.com>
2024-07-23 14:32:24 +0530

    Add files via upload

---
+++ src/com/pyro/testController/EmployeeTestController.java
@@ -0,0 +1,24 @@
+package com.pyro.testController;
+
+public class EmployeeTestController {
+	
+	public static void main(String[] args) {
+		
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+}
